d5f8083e270849cfe74339c6fe8789e865d35c1c
Dorian Leroy <dorian.leroy@cea.fr>
2023-11-30 16:50:54 +0100

    [misc] remove unused benchmarks

--- benchmark/PerfectGas/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -z $1 ]; then
-	echo "Usage: ./run.sh <path/to/build/dir>"
-elif [ -d $1 ]; then
-	ccc_mprun -p a100 -n 1 -c 32 $1/src/Mahyco -A,AcceleratorRuntime=cuda -A,MaxIteration=100 Data.arc
-else
-	echo "$1 does not exist or is not a directory"
-fi
